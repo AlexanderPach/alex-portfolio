6b1f54f61ddd51051cbb4d1b80ffa7c4cd58f4b0
AlexanderPach <apachofficial@gmail.com>
2023-09-18 03:07:14 -0700

    changed react-route dependency

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,6 @@
 import Image from 'next/image'
 import styles from './page.module.css'
 import NavBar from '@/components/NavBar'
-import { Routes, Route } from 'react-router-dom'
 import LinkList from '@/components/LinkList'
 import { Box, ChakraProvider } from '@chakra-ui/react'
 import HomePage from '@/components/HomePage'
